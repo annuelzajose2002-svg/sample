d82d1e5558160785cc79da5da10ae9044537a775
GIT123 <annuelzajose2002@gmail.com>
2026-05-05 12:48:20 +0530

    multiplication of two numbers

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ a=int(input("Enter First Number:"))
 b=int(input("Enter Second Number:"))
 c=a+b
 print(f"sum of {a} and {b} is :{c}")
+#multiplication of two numbers 
+a=int(input("Enter First Number:"))
+b=int(input("Enter Second Number:"))
+c=a*b
+print(f"multiplication of {a} and {b} is :{c}")
